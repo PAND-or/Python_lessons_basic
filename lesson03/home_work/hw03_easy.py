12b728fab3b18b1c746c4ebdf14e80f9574661e9
PAND-or <PAND-or@yandex.ru>
2018-09-13 16:21:24 +0300

    hw03 easy + normal

--- lesson03/home_work/hw03_easy.py
+++ lesson03/home_work/hw03_easy.py
@@ -1,3 +1,5 @@
+__author__ = "Андрей Петров"
+
 # Задание-1:
 # Напишите функцию, округляющую полученное произвольное десятичное число
 # до кол-ва знаков (кол-во знаков передается вторым аргументом).
@@ -5,8 +7,33 @@
 # Для решения задачи не используйте встроенные функции и функции из модуля math.
 
 def my_round(number, ndigits):
-    pass
+    
+    str_number = str(number)
+    
+    dot_index = str_number.find(".")
+    integer_number = str_number[:dot_index]
+    dec_number = str_number[dot_index+1:]
+    
+      
+    list_number = [int(i) for i in dec_number] #преобразование в список
+    j = 0
+    for k, num,  in enumerate(reversed(list_number)): #проход по списку в обратном порядке
+        if j > 0: 
+            num += 1
+            j = 0
+        if (num >= 10) & (len(list_number) - k - 2 >= 0):
+                list_number[-k-2] += 1
+                num = num - 10
+        elif(num >= 10) & (ndigits == 0):
+            integer_number = int(integer_number) + 1
+        if k < len(list_number) - ndigits: #До какой точности считаем + 0 и точка 
+            list_number[-k-1] = 0
+            j = 1 if int(num) > 5 else 0
+        else:
+            list_number[-k-1] = num
 
+
+    return int(integer_number) + int(''.join(str(x) for x in list_number))/(10**len(list_number))
 
 print(my_round(2.1234567, 5))
 print(my_round(2.1999967, 5))
@@ -20,9 +47,18 @@ print(my_round(2.9999967, 5))
 # !!!P.S.: функция не должна НИЧЕГО print'ить
 
 def lucky_ticket(ticket_number):
-    pass
-
-
+    #str_number = str(ticket_number)
+    list_number = [int(i) for i in str(ticket_number)] 
+    lenght = len(list_number)
+    if(lenght % 2 == 0):
+        sumone = sum(list_number[:int(lenght / 2)])
+        sumtwo = sum(list_number[int(lenght / 2):])
+        return True if sumone == sumtwo else  False
+    else:
+        sumone = sum(list_number[:int((lenght-1) / 2)])
+        sumtwo = sum(list_number[int((lenght+1) / 2):])
+        return True if sumone == sumtwo else  False
+    
 print(lucky_ticket(123006))
 print(lucky_ticket(12321))
 print(lucky_ticket(436751))
--- lesson03/home_work/hw03_normal.py
+++ lesson03/home_work/hw03_normal.py
@@ -1,9 +1,24 @@
+__author__ = "Андрей Петров"
+
 # Задание-1:
 # Напишите функцию, возвращающую ряд Фибоначчи с n-элемента до m-элемента.
 # Первыми элементами ряда считать цифры 1 1
 
 def fibonacci(n, m):
-    pass
+    feb = 0
+    list_feb = [1,1]
+    i = 1
+    n_index = 0
+    while feb < m:
+        print('i', i)
+        feb = list_feb[i] + list_feb[i-1]
+        list_feb.append(feb)
+        i += 1
+        if n > list_feb[i]:
+            n_index = i+1
+    return list_feb[n_index:]
+
+print(fibonacci(10,100))
 
 # Задача-2:
 # Напишите функцию, сортирующую принимаемый список по возрастанию.
@@ -12,16 +27,65 @@ def fibonacci(n, m):
 
 
 def sort_to_max(origin_list):
-    pass
+    n = 1 
+    while n < len(origin_list):
+         for i in range(len(origin_list)-n):
+              if origin_list[i] > origin_list[i+1]:
+                   origin_list[i],origin_list[i+1] = origin_list[i+1],origin_list[i]
+         n += 1
+    return origin_list
 
-sort_to_max([2, 10, -12, 2.5, 20, -11, 4, 4, 0])
+print(sort_to_max([2, 10, -12, 2.5, 20, -11, 4, 4, 0]))
 
 # Задача-3:
 # Напишите собственную реализацию стандартной функции filter.
 # Разумеется, внутри нельзя использовать саму функцию filter.
 
+a = [1, -4, 6, 8, -10]
+def func(x):
+    if x > 0:
+        return 1
+    else:
+        return 0
+    
+def my_filter(funcion, listing):
+    newlist = []
+    for i, val in enumerate(listing):
+        if funcion(val):
+            newlist.append(val)
+    return newlist
+    
+print(list(filter(func, a)))
+print(my_filter(func, a))
+
 
 # Задача-4:
 # Даны четыре точки А1(х1, у1), А2(x2 ,у2), А3(x3 , у3), А4(х4, у4).
 # Определить, будут ли они вершинами параллелограмма.
+
+my_points = ((0,1), (0,0), (1,0), (1,1))
+
+print(my_points)
+
+def check_squard(points):
+    # Паралеллограм это четырех угольник у которого 2 стороны парралельны друг другу
+    # построим 
+    if(len(points) != 4):
+        return False
+    lines = []
+    match = 0
+    for i, point in enumerate(points):
+        j = i+1
+        while j < 4: #построение графов 
+            another_point = points[j]
+            try:
+                k =  int((point[1] - another_point[1]) / (point[0] - another_point[0])) #угловые кооэфициенты прямых
+            except ZeroDivisionError:
+                k = 'inf'
+            lines.append(k)
+            if lines.count(k) == 2: #если есть 2 парралельные прямые
+                match += 1
+            j +=1
+    return True if match == 2 else False #2 парралельные прямые Бинго
+check_squard(my_points)
 
