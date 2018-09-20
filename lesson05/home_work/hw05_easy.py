173bfe45fb1300dfa8f590bb1b28451c5191638c
PAND-or <PAND-or@yandex.ru>
2018-09-20 15:38:50 +0300

    hw05 easy + normal

--- lesson05/home_work/hw05_easy.py
+++ lesson05/home_work/hw05_easy.py
@@ -1,10 +1,36 @@
+__author__ = "Андрей Петров"
+
 # Задача-1:
 # Напишите скрипт, создающий директории dir_1 - dir_9 в папке,
 # из которой запущен данный скрипт.
 # И второй скрипт, удаляющий эти папки.
+import os
+import shutil
+import inspect
+
+def create_dir():
+    if not((os.path.exists('dir_1')) & (os.path.exists('dir_2'))):
+        os.makedirs('dir_1')
+        os.makedirs('dir_2')
+def remove_dir():
+    if ((os.path.exists('dir_1')) & (os.path.exists('dir_2'))):
+        os.rmdir('dir_1')
+        os.rmdir('dir_2')
+
+create_dir()
+remove_dir()
 
 # Задача-2:
 # Напишите скрипт, отображающий папки текущей директории.
 
+def view_dir():
+    path = os.path.curdir
+    for root, dirs, files in  os.walk(path):
+        print(root)
+view_dir()
+
 # Задача-3:
 # Напишите скрипт, создающий копию файла, из которого запущен данный скрипт.
+
+current = os.path.abspath(inspect.getsourcefile(lambda:0))
+shutil.copy(current, r'copy_file.py')
--- lesson05/home_work/hw05_normal.py
+++ lesson05/home_work/hw05_normal.py
@@ -1,3 +1,5 @@
+__author__ = "Андрей Петров"
+
 # Задача-1:
 # Напишите небольшую консольную утилиту,
 # позволяющую работать с папками текущей директории.
@@ -13,3 +15,66 @@
 # Для решения данной задачи используйте алгоритмы из задания easy,
 # оформленные в виде соответствующих функций,
 # и импортированные в данный файл из easy.py
+
+import os
+
+
+def create_dir(new_dir):
+    if not(os.path.exists(new_dir)):
+        os.makedirs(new_dir)
+        return True
+    else:
+        return False
+def remove_dir(del_dir):
+    if os.path.exists(del_dir):
+        os.rmdir(del_dir)
+        return True
+    else:
+        return False
+
+def view_dir(path):
+    return os.listdir(path)
+
+print(
+    'Выберете действие \n',
+    '1 Перейти в папку\n',
+    '2 Посмотреть содержимое текущей папки\n',
+    '3 Удалить папку\n',
+    '4 Создать папку\n',
+    'q Для выхода',
+    
+)
+path = ''
+while True:
+    print('Текущая папка', path)
+    key = input('Введите команду: ')
+
+    if len(path) == 0:
+        path = os.path.curdir
+        #path = os.path.abspath
+    if key == '1':
+        new_path = input('Введите путь: ')
+        if os.path.exists(new_path):
+            path = new_path
+        else:
+            print('Директория {} не найдена'.format(path))
+    elif key == '2':
+        dirs = view_dir(path)
+        if len(dirs) > 0:
+            print('Сожержимое текущей папки', dirs)
+        else:
+            print('Папка пустая')
+    elif key == '3':
+        del_path = input('Введите имя папки для удаления: ')
+        if remove_dir(del_path):
+            print('Директория {} удалена'.format(del_path))
+        else:
+            print('Директория {} не найдена'.format(del_path))
+    elif key == '4':
+        new_path = input('Введите имя папки для создания: ')
+        if create_dir(new_path):
+            print('Директория {} создана'.format(new_path))
+        else:
+            print('Директория {} уже есть'.format(new_path))
+    elif key == 'q':
+        sys.exit() 
