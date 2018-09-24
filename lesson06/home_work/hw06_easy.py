6fa59eaa8d34eb387fac5de1812598b5c2b82931
PAND-or <PAND-or@yandex.ru>
2018-09-24 23:23:58 +0300

    same style fix

--- lesson06/home_work/hw06_easy.py
+++ lesson06/home_work/hw06_easy.py
@@ -1,26 +1,42 @@
+__author__ = "Андрей Петров"
+
 # Задача-1: Написать класс для фигуры-треугольника, заданного координатами трех точек.
 # Определить методы, позволяющие вычислить: площадь, высоту и периметр фигуры.
 
 
 import math
-class Triangle:
-   
+
+class Triangle: 
     def __init__(self, a, b, c):
         self.a = a
         self.b = b
         self.c = c
+        
+    def lenth(self, n, m):
+        return math.sqrt((m[0] - n[0])**2 + (m[1] - n[1])**2)
+    
+    @property
     def area(self):
-        return 0.5 * abs((self.a[0]- self.c[0]) * (self.b[1] - self.c[1]) - (self.b[0] - self.c[0])*(self.a[1] - self.c[1]))
+        ab = self.lenth(self.a, self.b)
+        bc = self.lenth(self.b, self.c)
+        ac = self.lenth(self.a, self.c)
+        p = self.perimeter / 2
+        return math.sqrt(p*(p - ac) * (p - ab) * (p - bc))
+    
+    @property
     def perimeter(self):
-        return math.sqrt((self.b[0] - self.a[0])**2 + (self.b[1] - self.a[1])**2) +  math.sqrt((self.c[0] - self.b[0])**2 + (self.c[1] - self.b[1])**2) +  math.sqrt((self.c[0] - self.a[0])**2 + (self.c[1] - self.a[1])**2)
+        ab = self.lenth(self.a, self.b)
+        bc = self.lenth(self.b, self.c)
+        ac = self.lenth(self.a, self.c)
+        return ab + bc + ac
+    
+    @property
     def height(self):
-        return 2 * self.area() / math.sqrt((self.b[0] - self.a[0])**2 + (self.b[1] - self.a[1])**2)
-
-triangle1 = Triangle((0,0), (0, 10), (10, 0))
-print(triangle1.b)
-print(triangle1.area())
-print(triangle1.perimeter())
-print(triangle1.height())
+        ab = self.lenth(self.a, self.b)
+        bc = self.lenth(self.b, self.c)
+        ac = self.lenth(self.a, self.c)
+        p = self.perimeter / 2
+        return (2 / ac) * math.sqrt(p*(p - ac) * (p - ab) * (p - bc))
 
 # Задача-2: Написать Класс "Равнобочная трапеция", заданной координатами 4-х точек.
 # Предусмотреть в классе методы:
@@ -33,27 +49,41 @@ class Trapeze:
         self.b = b
         self.c = c
         self.d = d
+        
     def lenth(self, n, m):
         return math.sqrt((m[0] - n[0])**2 + (m[1] - n[1])**2)
+    
+    @property
     def area(self):
         ad = self.lenth(self.a, self.d)
         bc = self.lenth(self.b, self.c)
-        return 0.5 * self.height() * (ad + bc)
+        return 0.5 * self.height * (ad + bc)
+    
+    @property
     def perimeter(self):
         ab = self.lenth(self.a, self.b)
         bc = self.lenth(self.b, self.c)
         cd = self.lenth(self.c, self.d)
         ad = self.lenth(self.a, self.d) 
         return ab + bc + cd + ad
+    
+    @property
     def height(self):
         ab = self.lenth(self.a, self.b)
         bc = self.lenth(self.b, self.c)
         cd = self.lenth(self.c, self.d)
         ad = self.lenth(self.a, self.d)
         return math.sqrt(abs(ab**2 - (((ad - bc)**2 + ab**2 + cd**2)/(2*(ad - bc)))**2))
+    
+    
+if __name__ == "__main__":
+    triangle1 = Triangle((0,0), (0, 10), (10, 0))
+    print('Площадь треугольника', triangle1.area)
+    print('Периметр треугольника', triangle1.perimeter)
+    print('Высота треугольника', triangle1.height)
 
-trapeze1 = Trapeze((0,0), (0, 10), (10, 10), (20, 0))
-print(trapeze1.b)
-print(trapeze1.area())
-print(trapeze1.perimeter())
-print(trapeze1.height())
+
+    trapeze1 = Trapeze((0,0), (0, 10), (10, 10), (20, 0))
+    print('Площадь трапеции', trapeze1.area)
+    print('Периметр трапеции', trapeze1.perimeter)
+    print('Высота трапеции', trapeze1.height)
--- lesson06/home_work/hw06_normal.py
+++ lesson06/home_work/hw06_normal.py
@@ -1,3 +1,5 @@
+__author__ = "Андрей Петров"
+
 # Задание-1:
 # Реализуйте описаную ниже задачу, используя парадигмы ООП:
 # В школе есть Классы(5А, 7Б и т.д.), в которых учатся Ученики.
@@ -16,43 +18,39 @@
 # 4. Узнать ФИО родителей указанного ученика
 # 5. Получить список всех Учителей, преподающих в указанном классе
 
-class Scool:
+class School:
     def __init__(self):
         self.teachers = []
         self.classes = []
 
-    def getClasses(self):
-        all_classes = [i.name for i in self.classes]
-        return all_classes
-    
-    def getClass(self, room_name):
+    def getClassByName(self, room_name):
         for i in self.classes:
             if i.name == room_name:
                 return i
-        else:
-            return None
-    
-    def getStudents(self, room_name):
-        return scool.getClass(room_name).getStudents()
-    
-    def getTeachers(self, room_name):
-        return scool.getClass(room_name).getTeachers()
+        return None
 
-    
+    def getClasses(self):
+        return [i.name for i in self.classes]
+
+    def getStudents(self, room_name):
+        return school.getClassByName(room_name).getStudents()
+
+    def getTeachers(self, room_name):
+        return school.getClassByName(room_name).getTeachers()
+
+
 class ClassRoom:
     def __init__(self, name):
         self.name = name
         self.students = []
         self.teachers = []
-        scool.classes.append(self)
+        school.classes.append(self)
     
     def getStudents(self):
-        all_students = [i.get_full_name() for i in self.students]
-        return all_students
+        return [i.get_full_name() for i in self.students]
 
     def getTeachers(self):
-        all_teachers = [i.get_full_name() for i in self.teachers]
-        return all_teachers
+        return [i.get_full_name() for i in self.teachers]
 
     
 class People:
@@ -68,36 +66,35 @@ class Student(People):
     def __init__(self, name, surname, class_room, parents):
         People.__init__(self, name, surname)
         self.parents = [People(i[0], i[1]) for i in parents]
-        if scool.getClass(class_room) == None:
+        if school.getClassByName(class_room) == None:
             self.class_room = ClassRoom(class_room)
         else: 
-            self.class_room = scool.getClass(class_room) 
+            self.class_room = school.getClassByName(class_room) 
         self.class_room.students.append(self)
 
     def listSubjects(self):
-        all_subjects = [i.courses for i in self.class_room.teachers]
-        return all_subjects
+        return [i.courses for i in self.class_room.teachers]
     
     def getParrents(self):
-        all_parents = [i.get_full_name() for i in self.parents]
-        return all_parents
+        return [i.get_full_name() for i in self.parents]
 
     
 class Teacher(People):
     def __init__(self, name, surname, teach_classes, courses):
         People.__init__(self, name, surname)
         self.teach_classes = []
-        for teach_class in teach_classes: #добавление этого учителя во все классы в которых он преподает
-            if scool.getClass(teach_class) == None:
-                ClassRoom(teach_class).teachers.append(self)
-            else: 
-                scool.getClass(teach_class).teachers.append(self)
         self.courses = courses
         self.teach_classes = teach_classes
-        scool.teachers.append(self)
+        for teach_class in teach_classes: 
+            if school.getClassByName(teach_class) == None:
+                ClassRoom(teach_class).teachers.append(self)
+            else: 
+                school.getClassByName(teach_class).teachers.append(self)
+        school.teachers.append(self)
+
 
 if __name__ == "__main__":
-    scool = Scool()
+    school = School()
     
     students = [Student("Александр", "Иванов", "5 А", [("Андрей", "Иванов"), ("Алла", "Иванова")]),
                 Student("Петр", "Сидоров", "8 Б", [("Петр", "Сидоров"), ("Ирина", "Сидорова")]),
@@ -113,8 +110,8 @@ if __name__ == "__main__":
                 Teacher("Иван", "Тетерев", ["4 В"], "Труд"),
                 ]
     
-    print('Список классов школы', scool.getClasses())
-    print('Список студентов 8Б', scool.getStudents("8 Б"))
+    print('Список классов школы', school.getClasses())
+    print('Список студентов 8Б', school.getStudents("8 Б"))
     print('Список предметов первого студента', students[0].listSubjects())
     print('Фио родителей первого студента', students[0].getParrents())
-    print('Список учителей 5А класса', scool.getTeachers("5 А"))
+    print('Список учителей 5А класса', school.getTeachers("5 А"))
